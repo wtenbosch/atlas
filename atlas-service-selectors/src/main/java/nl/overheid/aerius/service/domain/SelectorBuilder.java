fb92c3f52ba7e9a62be09f0858bdfaf6309e9779
Jacob Bijsterbosch <info@jacob-bijsterbosch.nl>
2020-07-20 14:26:50 +0200

    Add some more selector contructors to SelectorBuilder (#1)

--- atlas-service-selectors/src/main/java/nl/overheid/aerius/service/domain/SelectorBuilder.java
+++ atlas-service-selectors/src/main/java/nl/overheid/aerius/service/domain/SelectorBuilder.java
@@ -8,15 +8,24 @@ import nl.overheid.aerius.shared.domain.ServiceSelector;
 import nl.overheid.aerius.shared.domain.ServiceSelectorConfiguration;
 
 public class SelectorBuilder {
+  public static ServiceSelector.Builder selector(final String name, final String value) {
+    return selector(name, value, false);
+  }
+  
   public static ServiceSelector.Builder selector(final String name, final String value, final ServiceSelector.Builder... subs) {
     return selector(name, value, false, subs);
   }
+  
   public static ServiceSelector.Builder selector(final String name, final String value, final boolean defaultt, final ServiceSelector.Builder... subs) {
+    return selector(name, value, defaultt, list(subs));
+  }
+  
+  public static ServiceSelector.Builder selector(final String name, final String value, final boolean defaultt, final List<ServiceSelector> list) {
     return ServiceSelector.builder()
         .name(name)
         .value(value)
         .defaultt(defaultt)
-        .selectors(list(subs));
+        .selectors(list);
   }
 
   public static List<ServiceSelector> list(final ServiceSelector.Builder... selectors) {
