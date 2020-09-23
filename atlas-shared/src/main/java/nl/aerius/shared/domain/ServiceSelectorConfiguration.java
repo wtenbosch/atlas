e79a4577b9bb7a51c8d05ecee5bfb64776c8057e
JornC <jorncruijsen@gmail.com>
2020-09-23 15:07:50 +0200

    Make default count nullable/optional

--- atlas-shared/src/main/java/nl/aerius/shared/domain/ServiceSelectorConfiguration.java
+++ atlas-shared/src/main/java/nl/aerius/shared/domain/ServiceSelectorConfiguration.java
@@ -3,6 +3,8 @@ package nl.aerius.shared.domain;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 import com.google.auto.value.AutoValue;
 
 @AutoValue
@@ -20,6 +22,14 @@ public abstract class ServiceSelectorConfiguration implements Serializable {
 
   public abstract String description();
 
+  /**
+   * The number of default values.
+   * 
+   * This value exists so it doesn't need to be calculated.
+   */
+  @Nullable
+  public abstract Integer defaultNum();
+
   public abstract List<ServiceSelector> selectors();
 
   public abstract boolean multiselect();
@@ -31,6 +41,8 @@ public abstract class ServiceSelectorConfiguration implements Serializable {
     public abstract Builder title(String value);
 
     public abstract Builder description(String value);
+
+    public abstract Builder defaultNum(Integer value);
 
     public abstract Builder selectors(List<ServiceSelector> value);
 
